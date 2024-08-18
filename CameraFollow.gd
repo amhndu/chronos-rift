b1e5f9ba398cb202cf7a765b1fdf140d8153290c
Ayaskant Panigrahi <ayaskant.panigrahi@gmail.com>
2024-08-18 20:14:09 +0530

    feat: add camera zoom
    TODO Scroll actions not working with Input Map

--- CameraFollow.gd
+++ CameraFollow.gd
@@ -7,9 +7,34 @@ extends Camera3D
 @onready var objectToFollow = get_node(objectToFollowPath)
 
 @export var camAccel = 2 # Speed With Which The Camera Follows Up The Player (Not Need When Not using the `linear_interpolate` function
+@export var camZoomAccel = 2 # Speed With Which The Camera Zooms (Not needed when not using lerp)
+@export var camMinZoomSize = 10
+@export var camMaxZoomSize = 40
+@export var shouldUseLerp = true
+
+@onready var camera: Camera3D = get_parent().get_node("Camera")
+@onready var camTargetZoomSize = camera.size
 
 func _process(delta):
 	if objectToFollow == null:
 		return
-	cameraPivot.global_position = cameraPivot.global_position.lerp(objectToFollow.global_position, delta * camAccel)
-	#cameraPivot.global_position = objectToFollow.global_position # Un-Comment This & Comment Above if you want camera to just follow the player without any smooth follow-up
+
+	# TODO Not working with Input Map
+	#if Input.is_action_pressed("zoom_in"):
+		#camTargetZoomSize = max(camTargetZoomSize - 1, camMinZoomSize)
+	#elif Input.is_action_pressed("zoom_out"):
+		#camTargetZoomSize = min(camTargetZoomSize + 1, camMaxZoomSize)
+
+	if shouldUseLerp:
+		cameraPivot.global_position = cameraPivot.global_position.lerp(objectToFollow.global_position, delta * camAccel)
+		camera.size = lerp(float(camera.size), float(camTargetZoomSize), delta * camZoomAccel)
+	else:
+		cameraPivot.global_position = objectToFollow.global_position
+		camera.size = camTargetZoomSize
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:
+			camTargetZoomSize = max(camTargetZoomSize - 1, camMinZoomSize)
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN and event.pressed:
+			camTargetZoomSize = min(camTargetZoomSize + 1, camMaxZoomSize)
